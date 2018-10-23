7280253dc873c0f434e7147fb6fa4a3b98e71d1c
Yuchen Qian <qyc1128@126.com>
2018-10-22 21:52:24 -0700

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+class Car {
+  park() {
+    return 'stopped';
+  }
+
+  drive() {
+    return 'started';
+  }
+}
+
+let car;
+beforeEach(() => {
+  console.log('a');
+  car = new Car();
+});
+
+describe('Car', () => {
+  it('test park', () => {
+    assert.equal(car.park(), 'stopped');
+  });
+
+  it('test drive', () => {
+    assert.equal(car.drive(), 'started');
+  });
+});
